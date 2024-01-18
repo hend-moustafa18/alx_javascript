9084de827b7e02578b360447dc516c3c19a850eb
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2024-01-18 15:49:34 +0200

    new

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,18 +1,16 @@
 #!/usr/bin/node
 const myObject = {
-    type: 'object',
-    value: 12
-  };
-  console.log(myObject);
-  
-  // Add the incr function using a method declaration
-  myObject.incr = function () {
-    this.value++; // Increment the value property using 'this'
-  };
-  
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
+  type: 'object',
+  value: 12,
+  incr: function () {
+    this.value++;
+  }
+};
+
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
