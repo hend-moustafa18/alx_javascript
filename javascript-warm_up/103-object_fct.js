0ca30c824917e90144603ceb3c703ff2ac03f504
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2024-01-18 15:37:15 +0200

    new

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,20 +1,17 @@
 #!/usr/bin/node
-const myObject = {
+let myObject = {
     type: 'object',
     value: 12
   };
-  
   console.log(myObject);
   
-  myObject.incr = function incr() {
+  myObject.incr = function () {
     this.value++;
   };
   
   myObject.incr();
   console.log(myObject);
-  
   myObject.incr();
   console.log(myObject);
-  
   myObject.incr();
   console.log(myObject);
