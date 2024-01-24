29ea328dc990ad21899792c0769b18bc1b8c7738
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2024-01-24 02:16:06 +0200

    new

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,15 @@
+function welcome(firstName, lastName) {
+    // Concatenate first name and last name with a space in between
+    var fullName = firstName + ' ' + lastName;
+
+    // Inner function to display the welcome message
+    function displayFullName() {
+        alert('Welcome ' + fullName + '!');
+    }
+
+    // Call the inner function to display the welcome message
+    displayFullName();
+}
+
+// Example usage:
+welcome('John', 'Doe');
